13b0067da2f9ef6fa85642eb05d0b3f24fe1d5f8
Margarito <jimenez8382@gmail.com>
2017-01-30 17:28:06 -0600

    Creating DbContext and Data models

---
+++ Expenses/Data/ExpensesDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Data.Entity;
+namespace Expenses.Data
+{
+    public class ExpensesDbContext : DbContext
+    {
+        public ExpensesDbContext() : base("name=MyContext")
+        {
+        }
+        public DbSet<Models.Expenses> Expense { get; set; }
+    }
+}
---
+++ Expenses/Data/Models/Expenses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses.Data.Models
+{
+    public class Expenses
+    {
+        public long Id { get; set; }
+        [Required]
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
+        [Required]
+        [Display(Name = "Expenses")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        public double Amount { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
+    public class ExpenseReport
+    {
+        public ExpenseReport()
+        {
+            Report = new List<Expenses>();
+            Year = System.DateTime.Now.Year;
+        }
+        [Required]
+        [Display(Name = "Fiscal Year")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        public int FiscalYear { get; set; }
+        public int Year { get; set; }
+        public List<Expenses> Report { get; set; }
+    }
+}
